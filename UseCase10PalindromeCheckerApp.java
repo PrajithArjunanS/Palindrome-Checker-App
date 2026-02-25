e76b1723d4fa5299aeb564fe63ba5c5b486db939
PrajithArjunanS <ps0663@srmist.edu.in>
2026-02-25 18:59:29 +0530

    UC10: Case-insensitive and space-ignored palindrome validation implemented

---
+++ UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,34 @@
+/**
+ * =====================================================
+ * MAIN CLASS - UseCase10PalindromeCheckerApp
+ * =====================================================
+ *
+ * Use Case 10: Case-Insensitive & Space-Ignored Palindrome
+ *
+ * Validates a palindrome after normalizing the string
+ * by removing spaces/symbols and converting to lowercase.
+ *
+ * @version 10.0
+ */
+
+public class UseCase10PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "A man a plan a canal Panama";
+
+        String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < normalized.length() / 2; i++) {
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
